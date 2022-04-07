2453ed0c0473ff71c7b9b3772064eb5e5d534588
KateChu <chu.shihchien@gmail.com>
2022-04-07 20:30:53 +0800

    feat: heroku

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,9 +3,7 @@ const router = express.Router();
 const User = require("../../models/User");
 
 router.get("/", async (req, res) => {
-  const userId = req.user._id;
-  const expenses = await User.findById(userId).populate("expenseId");
-  res.render("index", { expenses });
+  res.redirect("/expenses");
 });
 
 module.exports = router;
